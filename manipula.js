2868220881e7e6c1b1583bd388b395e2c9a761b0
Pedro <pedro_rique17@hotmail.com>
2021-10-22 16:46:38 -0300

    usando reduce

--- manipula.js
+++ manipula.js
@@ -8,3 +8,21 @@ gerentes = [];
 gerentes = funcionarios.filter(func =>(func.categoria === "gerente"));
 
 // console.log(gerentes);
+
+// Funcionario com menor id
+
+let func = funcionarios.reduce((anterior, atual) => {
+
+	console.log("Anterior: " + anterior.id);
+	console.log("Atual: " + atual.id);
+
+	if (atual.id < anterior.id)
+		return atual;
+	else
+		return anterior;
+
+}, {
+	id: 999999999999
+});
+
+//console.log(func);
