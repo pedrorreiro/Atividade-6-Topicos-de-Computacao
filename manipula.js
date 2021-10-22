c16ddc563b90e87effa9d57d04842f494c552952
Pedro <pedro_rique17@hotmail.com>
2021-10-22 16:44:48 -0300

    usando filter

--- manipula.js
+++ manipula.js
@@ -1,3 +1,10 @@
 const {
 	funcionarios
 } = require("./funcionarios.json");
+
+// Recuperar apenas Gerentes
+
+gerentes = [];
+gerentes = funcionarios.filter(func =>(func.categoria === "gerente"));
+
+// console.log(gerentes);
